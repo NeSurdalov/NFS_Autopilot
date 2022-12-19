7a1e1455edf519d1469b47e1d3fcf04cff7dfb1b
vitaemetus <bazaliy.daniil@gmail.com>
2022-12-19 19:49:10 +0300

    fixed steering a lil bit again

--- classes/Steering.py
+++ classes/Steering.py
@@ -4,10 +4,8 @@ class Steering:
         turn = (amount_r - amount_l) * (
                 np.exp(v / 220 - 1) * ((amount_r - amount_l) ** 2 / (amount_r + amount_l) ** 2)) / (
                         amount_r + amount_l)
-        if turn > 100:
-            turn = 100
-        if v == 0 and amount_l == amount_r == 0:
-            turn = 0.2
         if turn == None:
             turn = 0
+        elif turn > 100:
+            turn = 100
         return (turn * 100)
