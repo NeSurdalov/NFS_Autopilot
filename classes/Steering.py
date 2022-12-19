5c295049c4327663ecfc2ebb27862e3965f1fe2d
vitaemetus <bazaliy.daniil@gmail.com>
2022-12-19 20:06:33 +0300

    steerung fixed finally

--- classes/Steering.py
+++ classes/Steering.py
@@ -1,11 +1,12 @@
 import numpy as np
-class Steering:
-    def steering_amount(v, amount_l, amount_r):
-        turn = (amount_r - amount_l) * (
+
+def steering_amount(v, amount_l, amount_r):
+    turn = (amount_r - amount_l) * (
                 np.exp(v / 220 - 1) * ((amount_r - amount_l) ** 2 / (amount_r + amount_l) ** 2)) / (
                         amount_r + amount_l)
-        if turn == None:
-            turn = 0
-        elif turn > 100:
-            turn = 100
-        return (turn * 100)
+    print(turn, type(turn))
+    if np.isnan(turn):
+        turn = 0
+    elif turn > 1:
+        turn = 1
+    return (turn * 100)
