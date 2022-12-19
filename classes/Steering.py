5a74f8797d270e45867f741a19680f3341386c4d
vitaemetus <bazaliy.daniil@gmail.com>
2022-12-19 20:15:22 +0300

    steering back to initial function

--- classes/Steering.py
+++ classes/Steering.py
@@ -4,7 +4,6 @@ def steering_amount(v, amount_l, amount_r):
     turn = (amount_r - amount_l) * (
                 np.exp(v / 220 - 1) * ((amount_r - amount_l) ** 2 / (amount_r + amount_l) ** 2)) / (
                         amount_r + amount_l)
-    print(turn, type(turn))
     if np.isnan(turn):
         turn = 0
     elif turn > 1:
